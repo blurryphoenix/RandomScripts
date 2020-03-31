0b62bd7550e2de7045c16943fe3649c957a89396
Matt Erickson <Matt@MattErickson.me>
2020-03-31 10:32:18 -0500

    moving some functions around to make the code cleaner

---
+++ DedicatedServer/DSHelpers.ps1
@@ -0,0 +1,171 @@
+Function CheckOrInstallSteam {
+  param(
+    [string] $installLocation
+  )
+  
+  # start /wait srcds.exe -console -game garrysmod $gamenameArg $workshopGameArg
+  # 
+  Add-Type -AssemblyName System.IO.Compression.FileSystem
+  if (!(Test-Path $installLocation)) {
+    Invoke-WebRequest "http://media.steampowered.com/installer/steamcmd.zip" -outfile $env:USERPROFILE\Downloads\steamcmd.zip
+    [System.IO.Compression.ZipFile]::ExtractToDirectory("$env:USERPROFILE\Downloads\steamcmd.zip", $installLocation);
+  }
+}
+
+Function SetupMapRotation {
+  param(
+    [Parameter()]
+    [string]$gmodDir,
+    [Parameter()]
+    [string]$gameMode
+  )
+  #between fountain and arena  is supposed to be "Awesome Building - Prop Hunt"
+  echo $gameMode
+  if ($gameMode -eq 'prop_hunt') {
+    $maps = "ph_restaurant
+ph_nightofthelivingdead
+ph_office
+ph_fancyhouse
+ph_parkinglot
+ph_starship
+ph_funeral_home
+ph_fountain
+ph_arena
+ph_chinese
+ph_bank
+ph_saltfactory
+ph_cliffyard
+ph_motelblacke_redux
+ph_apartment_v2
+ph_libraryfactory
+ph_spieje
+ph_house_v3
+ph_mansion_v1
+ph_diordna_hotel_2
+ph_houseplace
+ph_gas_stationrc7_xmas
+ph_moonbase_redux
+ph_grand_hotel_night";
+  } elseif ($gameMode -eq 'terrortown') {
+    $maps = "ttt_nuclear_power_b2
+ttt_camel_v1
+ttt_old_factory
+ttt_sewer_system
+ttt_skycity4
+ttt_slender_v3_fix
+ttt_stargate
+ttt_theship_v1
+ttt_aircraft_v1b
+ttt_subway_b4
+ttt_minecraft_b5
+ttt_swimming_pool_v2
+ttt_traitorville_v2
+ttt_bb_teenroom_b2
+ttt_bb_outpost57_b5
+ttt_fastfood_a6
+ttt_community_bowling
+ttt_chaser
+ttt_icarus_r1_a2
+ttt_lttp_kakariko
+ttt_lumos
+ttt_mc_skyislands
+ttt_metropolis
+ttt_richland
+ttt_vessel
+ttt_terrortown
+ttt_casino_b2
+ttt_cluedo_b5_improved1
+ttt_oldruins
+ttt_moonbase_redux";
+  }
+  $maps | Set-Content $gmodDir\garrysmod\cfg\mapcycle.txt;
+  return $maps -Split '\r?\n';
+}
+
+Function SetupServerConfig {
+  param(
+    [Parameter()]
+    [string]$gmodDir
+  )
+  $serverConfig = '// Hostname for server.
+// RCON - remote console password.
+rcon_password "mutmatt_r0x!"
+
+// Server password - for private servers.
+sv_password "derp"
+
+// Server Logging
+log on
+sv_logbans 1
+sv_logecho 1
+sv_logfile 1
+sv_log_onefile 0
+lua_log_sv 0
+
+sv_rcon_banpenalty 0
+sv_rcon_maxfailures 20
+sv_rcon_minfailures 20
+sv_rcon_minfailuretime 20
+
+// Network Settings
+sv_downloadurl ""
+sv_loadingurl ""
+net_maxfilesize 64
+sv_maxrate 0
+sv_minrate 800000
+sv_maxupdaterate 66
+sv_minupdaterate 33
+sv_maxcmdrate 66
+sv_mincmdrate 33
+
+// Server Settings
+sv_airaccelerate 100
+sv_gravity 600
+sv_allow_voice_from_file 0
+sv_turbophysics 0
+sv_client_min_interp_ratio 1
+sv_client_max_interp_ratio 2
+think_limit 20
+sv_region 0
+sv_noclipspeed 5
+sv_noclipaccelerate 5
+sv_lan 0
+sv_alltalk 1
+sv_contact server_admin@matterickson.me
+sv_cheats 0
+sv_allowcslua 0
+sv_pausable 0
+sv_filterban 1
+sv_forcepreload 1
+sv_footsteps 1
+sv_voiceenable 1
+sv_timeout 120
+sv_deltaprint 0
+sv_allowupload 0
+sv_allowdownload 0
+
+// Sandbox Settings
+sbox_noclip 0
+sbox_godmode 0
+sbox_weapons 0
+sbox_playershurtplayers 0
+sbox_maxprops 100
+sbox_maxragdolls 50
+sbox_maxnpcs 10
+sbox_maxballoons 10
+sbox_maxeffects 0
+sbox_maxdynamite 0
+sbox_maxlamps 5
+sbox_maxthrusters 20
+sbox_maxwheels 20
+sbox_maxhoverballs 20
+sbox_maxvehicles 1
+sbox_maxbuttons 20
+sbox_maxemitters 0
+
+// Misc Config$
+exec banned_user.cfg
+exec banned_ip.cfg
+heartbeat';
+  $serverConfig | Set-Content $gmodDir\garrysmod\cfg\server.cfg;
+}
--- SetupAndRunGModDS.ps1
+++ SetupAndRunGModDS.ps1
@@ -1,150 +1,74 @@
+# 'murder','ttt','trouble in terrorist town','sandbox','objhunt'
 param(
   [Parameter()]
   [string]$game,
-  # 'murder','ttt','trouble in terrorist town','sandbox','objhunt'
   [Parameter()]
-  [string]$serverName = "Tucker_Smells",
+  [string]$serverName,
   [Parameter()]
   [string]$map,
   [Parameter()]
-  [string]$installLocation = "C:\sourceServer"
+  [string]$installLocation
 )
-$serverName = $serverName.trim()
-$serverName = $serverName.replace(' ', '_')
-$steamexe = "$installLocation\steamcmd.exe"
-$gmodDir = "$installLocation\steamapps\common\GarrysModDS"
-$gmod = "$gmodDir\srcds.exe"
-#raw server.cfg
-$serverConfig = '// Hostname for server.
-// RCON - remote console password.
-rcon_password "mutmatt_r0x!"
+. ".\DedicatedServer\DSHelpers.ps1"
 
-// Server password - for private servers.
-sv_password "derp"
-
-// Server Logging
-log on
-sv_logbans 1
-sv_logecho 1
-sv_logfile 1
-sv_log_onefile 0
-lua_log_sv 0
-
-sv_rcon_banpenalty 0
-sv_rcon_maxfailures 20
-sv_rcon_minfailures 20
-sv_rcon_minfailuretime 20
-
-// Network Settings
-sv_downloadurl ""
-sv_loadingurl ""
-net_maxfilesize 64
-sv_maxrate 0
-sv_minrate 800000
-sv_maxupdaterate 66
-sv_minupdaterate 33
-sv_maxcmdrate 66
-sv_mincmdrate 33
-
-// Server Settings
-sv_airaccelerate 100
-sv_gravity 600
-sv_allow_voice_from_file 0
-sv_turbophysics 0
-sv_client_min_interp_ratio 1
-sv_client_max_interp_ratio 2
-think_limit 20
-sv_region 0
-sv_noclipspeed 5
-sv_noclipaccelerate 5
-sv_lan 0
-sv_alltalk 1
-sv_contact server_admin@matterickson.me
-sv_cheats 0
-sv_allowcslua 0
-sv_pausable 0
-sv_filterban 1
-sv_forcepreload 1
-sv_footsteps 1
-sv_voiceenable 1
-sv_timeout 120
-sv_deltaprint 0
-sv_allowupload 0
-sv_allowdownload 0
-
-// Sandbox Settings
-sbox_noclip 0
-sbox_godmode 0
-sbox_weapons 0
-sbox_playershurtplayers 0
-sbox_maxprops 100
-sbox_maxragdolls 50
-sbox_maxnpcs 10
-sbox_maxballoons 10
-sbox_maxeffects 0
-sbox_maxdynamite 0
-sbox_maxlamps 5
-sbox_maxthrusters 20
-sbox_maxwheels 20
-sbox_maxhoverballs 20
-sbox_maxvehicles 1
-sbox_maxbuttons 20
-sbox_maxemitters 0
-
-// Misc Config$
-exec banned_user.cfg
-exec banned_ip.cfg
-heartbeat';
-
-$workshopGameId;
+if (!$installLocation) {
+  $installLocation = "C:\sourceServer";
+}
+if (!$serverName) {
+  $serverName = "Tucker_Smells";
+}
 switch($game) {
-  'prop_hunt' {
+  "$_ -eq murder" {
+    $gameMode = "murder";
     break;
   }
-  'murder' {
+  "$_ -eq ttt" { continue; }
+  "$_ -eq tt" { continue; }
+  "$_ -eq trouble in terrorist town" {
+    $gameMode = "terrortown";
     break;
   }
-  'ttt' {
-    continue
+  "$_ -eq prop_hunt" { continue; }
+  "$_ -eq obj_hunt" { continue; }
+  "$_ -eq prophunt" { continue; }
+  "$_ -eq objhunt" {
+    $gameMode = "prop_hunt";
+    break;
   }
-  'trouble in terrorist town' {
-    $gamename = "terrortown";
+  default { 
+    $gameMode = "prop_hunt";
     break;
   }
 }
-$gamenameArg = if (!$game) { "+gamemode sandbox" } else { "+gamemode $game" }
+$gamenameArg = "+gamemode $gameMode"
+
+$serverName = $serverName.trim()
+$serverName = $serverName.replace(' ', '_')
+$installLocation = $installLocation.TrimEnd("\");
+$steamExe = "$installLocation\steamcmd.exe"
+$gmodDir = "$installLocation\steamapps\common\GarrysModDS"
+$gmodExe = "$gmodDir\srcds.exe"
+
 
 $networkArg = if (!$localNetworkCardIp) { "" } else { "-ip $localNetworkCardIp" }
 $workshopGameArg = "+host_workshop_collection 2040200286"
 
-# start /wait srcds.exe -console -game garrysmod $gamenameArg $workshopGameArg
-# 
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-function Unzip
-{
-    param([string]$zipfile, [string]$outpath)
+CheckOrInstallSteam -installLocation $installLocation;
+SetupServerConfig -gmodDir $gmodDir;
+$maps = SetupMapRotation -gmodDir $gmodDir -gameMode $gameMode;
 
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-}
-
-
-if (!(Test-Path $installLocation)) {
-  wget "http://media.steampowered.com/installer/steamcmd.zip" -outfile $env:USERPROFILE\Downloads\steamcmd.zip
-  Unzip $env:USERPROFILE\Downloads\steamcmd.zip C:\sourceServer
-}
-
-
+# 4020 is gmod
 $args = "+login anonymous +app_update 4020 validate +quit"
-Start-Process -NoNewWindow -Wait -FilePath $steamexe -ArgumentList $args 
+Start-Process -NoNewWindow -Wait -FilePath $steamExe -ArgumentList $args
 
 if (!$map) {
-  $map = Get-ChildItem $gmodDir\garrysmod\maps -Filter *.bsp -Recurse | Get-Random
+  $map = $maps | Get-Random
+  echo "hello world $map"
 }
-$serverConfig | Out-File $gmodDir\garrysmod\cfg\server.cfg -Encoding "ASCII"
 echo "resource.AddWorkshop("2040200286")" | Out-File $gmodDir\garrysmod\lua\autorun\server\workshop.lua -Encoding "ASCII"
 
 $args = "-console +hostname $serverName -authkey B69B3D3AAC2A179EA41E576C476BF8C4 $workshopGameArg -game garrysmod $gamenameArg $networkArg +exec server.cfg +map $map"
-Start-Process -NoNewWindow -FilePath $gmod -ArgumentList $args
+Start-Process -NoNewWindow -FilePath $gmodExe -ArgumentList $args
 echo "----------------------srcds.exe commands (the other window)---------------------------------------------------------"
 echo "| changelevel *map_name*                       | changes the map                                                   |"
 echo "| status                                       | gives information about the running server (including ip address) |"
@@ -152,6 +76,7 @@ echo "| quit                                         | exits (shut down the serv
 echo "----------------------in game dev console commands (~)--------------------------------------------------------------"
 echo "| rcon_password tH3_pw                         | drop into 'admin' mode                                            |"
 echo "| rcon changelevel *map_name* (ph, tt, etc)    | changes the level of the server                                   |"
-echo "| rcon gamemode *mode* (terrortown, prop_hunt) | changes the level of the server                                   |"
+echo "| rcon gamemode *mode* (terrortown, prop_hunt) | changes the game mode                                             |"
+echo "| you can test these commands as 'changelevel' to find the map and then use 'rcon' to change ther server map       |"
 echo "| also helpful link for commands https://steamcommunity.com/sharedfiles/filedetails/?id=170589737                  |"
 echo "--------------------------------------------------------------------------------------------------------------------"
